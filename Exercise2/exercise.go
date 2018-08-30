f45908e56a68e11abd821cb374a39994d857d1b4
Veeru Natarajan <veeruns@yahoo.com>
2018-08-30 14:25:05 -0700

    ADding Exercises

---
+++ Exercise2/exercise.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	args := os.Args[1:]
+	sum := 0.0
+	for _, val := range args {
+		num, err := strconv.ParseFloat(val, 64)
+		if err == nil {
+			fmt.Printf("Value is %f\n", num)
+			sum = sum + num
+		}
+	}
+	fmt.Printf("Sum of all the integers is %d\n", sum)
+}
